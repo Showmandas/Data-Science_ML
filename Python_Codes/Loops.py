3a99b9c2e190c36364e9d28724d10193df82c7f8
Showmandas <showmandas3216@gmail.com>
2026-03-06 00:21:32 +0600

    Loops

---
+++ Python_Codes/Loops.py
@@ -0,0 +1,26 @@
+nuts=["chocolate","dairy milk","kitkat","glaze"]
+
+#for loop
+for i in nuts:
+    if i == "kitkat":
+       print(f"I don't like {i}") 
+    else:
+        print(f"I love {i}")
+
+
+#while loop
+
+x=0
+
+while x < 90:
+    print(f"angle is {x}")
+    x+=30
+
+
+for a in range(0,91,20):
+    print(a)
+
+
+for i in range(10,0,-1):
+    print(i)
+

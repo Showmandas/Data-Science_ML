6f8cd6664019a2600ca95c16614b5e7f0b37f701
Showmandas <showmandas3216@gmail.com>
2026-03-05 23:00:22 +0600

    Basic operation

---
+++ Python_Codes/Basic operation.py
@@ -0,0 +1,26 @@
+#basic operation
+x=3456
+y=0.56
+z=x*y
+# print("Result",z)
+
+chocolate=3
+sprinkle=2
+price_of_chocolate=5
+price_of_sprinkle=6
+t_cost=chocolate*price_of_chocolate + sprinkle*price_of_sprinkle
+# print("Total Cost",t_cost); 
+
+vat = t_cost*0.1
+final_cost=t_cost + vat
+# print("Final cost: ",final_cost)
+
+slice_of_burger=8
+people=3
+
+slice_per_people=slice_of_burger // people
+# print("Each people gets : ",slice_per_people)
+
+# power
+power=2**3
+print(power)

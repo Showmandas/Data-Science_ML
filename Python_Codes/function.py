ee45951e3605b871f7b1b916a289de34c8a88f6e
Showmandas <showmandas3216@gmail.com>
2026-03-10 00:10:30 +0600

    function

---
+++ Python_Codes/function.py
@@ -0,0 +1,16 @@
+def my_func(name):
+    print("My name is",name)
+
+my_func("showman")
+
+def fullName(f_name,l_name):
+    print(f_name,l_name)
+
+fullName("Bunny","das")
+
+
+def calculator(x):
+    return x * 117
+
+y=calculator(10)
+print(y)

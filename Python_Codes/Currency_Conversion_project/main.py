3ea6e55715f38785eb88a83c764cae9a793c8ae1
Showmandas <showmandas3216@gmail.com>
2026-03-14 15:37:04 +0600

    Make a curreny conversion project

---
+++ Python_Codes/Currency_Conversion_project/main.py
@@ -0,0 +1,17 @@
+
+print(
+    """Here is a list of global currencies : \n 
+1.USD: US Dollar ($)
+2.EUR: Euro (€)
+3.GBP: British Pound Sterling (£)
+4.JPY: Japanese Yen (¥)
+5.AUD: Australian Dollar ($)
+6.CAD: Canadian Dollar ($)
+7.CHF: Swiss Franc (Fr)
+8.CNY: Chinese Yuan (¥)
+9.INR: Indian Rupee (₹)
+10.RUB: Russian Ruble (₽)"""
+)
+
+n=int(input("Choose currency for converting to Taka(৳) from the list by entering 1/2/3... : " ))
+
